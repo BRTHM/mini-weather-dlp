2469dcea4890821c821abe20563c163fc6ec41d2
BRTHM <181210489@pku.edu.cn>
2018-10-20 19:46:09 +0800

    init project

--- app/src/main/java/com/example/asdf/miniweather/MainActivity.java
+++ app/src/main/java/com/example/asdf/miniweather/MainActivity.java
@@ -1,12 +1,229 @@
 package com.example.asdf.miniweather;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity{
+import com.example.asdf.com.example.asdf.bean.TodayWeather;
+import com.example.asdf.util.NetUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
+
+
+public class MainActivity extends Activity implements OnClickListener{
+    private static final int UPDATE_TODAY_WEATHER = 1;
+    private ImageView mUpdateBtn;
+    private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv, climateTv, windTv, city_name_Tv;
+    private ImageView weatherImg, pmImg;
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case UPDATE_TODAY_WEATHER:
+                    updateTodayWeather((TodayWeather) msg.obj);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
     protected void onCreate(Bundle  savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        mUpdateBtn=(ImageView)findViewById(R.id.title_updata_btn);
+        mUpdateBtn.setOnClickListener(this);
+        if(NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE)
+        {
+            Log.d("myweather", "net connected");
+            Toast.makeText(MainActivity.this,"net connected",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Log.d("myweather", "net unconnected");
+            Toast.makeText(MainActivity.this,"net unconnected",Toast.LENGTH_LONG).show();
+        }
+        initView();
     }
+
+    void initView() {
+        city_name_Tv = (TextView) findViewById(R.id.title_city_name);
+        cityTv = (TextView) findViewById(R.id.city);
+        timeTv = (TextView) findViewById(R.id.time);
+        humidityTv = (TextView) findViewById(R.id.humidity);
+        weekTv = (TextView) findViewById(R.id.week_today);
+        pmDataTv = (TextView) findViewById(R.id.pm_data);
+        pmQualityTv = (TextView) findViewById(R.id.pm2_5_quality);
+        pmImg = (ImageView) findViewById(R.id.pm2_5_img);
+        temperatureTv = (TextView) findViewById(R.id.temperature);
+        climateTv = (TextView) findViewById(R.id.climate);
+        windTv = (TextView) findViewById(R.id.wind);
+        weatherImg = (ImageView) findViewById(R.id.weather_img);
+        city_name_Tv.setText("N/A");
+        cityTv.setText("N/A");
+        timeTv.setText("N/A");
+        humidityTv.setText("N/A");
+        pmDataTv.setText("N/A");
+        pmQualityTv.setText("N/A");
+        weekTv.setText("N/A");
+        temperatureTv.setText("N/A");
+        climateTv.setText("N/A");
+        windTv.setText("N/A");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.title_updata_btn)
+        {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_ city_code", "101010100");
+            Log.d("myWeather",cityCode);
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE)
+            {
+                Log.d("myWeather", "网络OK");
+                queryWeatherCode(cityCode);
+            }
+            else
+            {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void queryWeatherCode(String cityCode) {
+        final String addressWeather = "https://free-api.heweather.com/s6/weather/forecast?location=CN" + cityCode+"&key=6393f5e642b943748dc79a31822f936e";
+        final String addressAir = "https://free-api.heweather.com/s6/air/now?location=CN" + cityCode+"&key=6393f5e642b943748dc79a31822f936e";
+        Log.d("myWeather", addressWeather);
+        Log.d("myWeather", addressAir);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TodayWeather todayWeather=null;
+                String jsonDataWeather = getResponseStr(addressWeather);
+                String jsonDataAir = getResponseStr(addressAir);
+                String jsonData="["+jsonDataWeather+","+jsonDataAir+"]";
+                todayWeather=parseJSON(jsonData);
+                if (todayWeather != null) {
+                    Log.d("myWeather", todayWeather.toString());
+                    Message msg = new Message();
+                    msg.what = UPDATE_TODAY_WEATHER;
+                    msg.obj = todayWeather;
+                    mHandler.sendMessage(msg);
+
+                }
+
+
+
+            }
+        }).start();
+    }
+
+    private String getResponseStr(String address)
+    {
+        HttpURLConnection con = null;
+        String responseStr=null;
+        try {
+            URL url = new URL(address);
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(8000);
+            con.setReadTimeout(8000);
+            InputStream in = con.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder response = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                response.append(str);
+                Log.d("myWeather", str);
+            }
+            responseStr = response.toString();
+            Log.d("myWeather", responseStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
+        }
+        return responseStr;
+    }
+
+
+    private TodayWeather parseJSON(String jsonData)
+    {
+        TodayWeather todayWeather = new TodayWeather();
+        try {
+            JSONArray jsonArray=new JSONArray(jsonData);
+            JSONObject jsonObjectWeather=jsonArray.getJSONObject(0).getJSONArray("HeWeather6").getJSONObject(0);
+            JSONObject jsonObjectWeatherForecast=jsonObjectWeather.getJSONArray("daily_forecast").getJSONObject(0);
+            JSONObject jsonObjectAir=jsonArray.getJSONObject(1).getJSONArray("HeWeather6").getJSONObject(0);
+            todayWeather.setCity(jsonObjectWeather.getJSONObject("basic").getString("location"));
+            todayWeather.setUpdatetime(jsonObjectWeather.getJSONObject("update").getString("utc"));
+            String tempMin=jsonObjectWeatherForecast.getString("tmp_min");
+            String tempMax=jsonObjectWeatherForecast.getString("tmp_max");
+            todayWeather.setWendu(tempMin+"°C-"+tempMax+"°C");
+            todayWeather.setShidu(jsonObjectWeatherForecast.getString("hum"));
+            todayWeather.setPm25(jsonObjectAir.getJSONObject("air_now_city").getString("pm25"));
+            todayWeather.setQuality(jsonObjectAir.getJSONObject("air_now_city").getString("qlty"));
+            todayWeather.setFengxiang(jsonObjectWeatherForecast.getString("wind_dir"));
+            todayWeather.setFengli(jsonObjectWeatherForecast.getString("wind_sc"));
+            todayWeather.setDate(jsonObjectWeatherForecast.getString("date"));
+            todayWeather.setLow(tempMin);
+            todayWeather.setHigh(tempMax);
+            String typeDay=jsonObjectWeatherForecast.getString("cond_txt_d");
+            String typeNight=jsonObjectWeatherForecast.getString("cond_txt_n");
+            todayWeather.setType(typeDay.equals(typeNight)?typeDay:typeDay+"转"+typeNight);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return todayWeather;
+    }
+
+    void updateTodayWeather(TodayWeather todayWeather) {
+        city_name_Tv.setText(todayWeather.getCity() + "天气");
+        cityTv.setText(todayWeather.getCity());
+        timeTv.setText(todayWeather.getUpdatetime().split(" ")[1] + "发布");
+        humidityTv.setText("湿度：" + todayWeather.getShidu()+"%");
+        pmDataTv.setText(todayWeather.getPm25());
+        pmQualityTv.setText(todayWeather.getQuality());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date=null;
+        try {
+            date = dateFormat.parse(todayWeather.getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        dateFormat=new SimpleDateFormat("MM月dd日 EEEE",Locale.CHINESE);
+        weekTv.setText(dateFormat.format(date));
+        temperatureTv.setText(todayWeather.getLow() + "°C~" + todayWeather.getHigh()+"°C");
+        climateTv.setText(todayWeather.getType());
+        windTv.setText("风力:" + todayWeather.getFengli()+"级");
+        Toast.makeText(MainActivity.this, "更新成功！", Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/asdf/util/Util.java
@@ -0,0 +1,4 @@
+package com.example.asdf.util;
+
+public class Util {
+}
